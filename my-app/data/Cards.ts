8f3b0ceaec1f4cb9a2e8f97821d1242e3c0be989
AlvaroNegrin <usr3856@salesianos-lacuesta.net>
2024-11-11 21:02:02 +0000

    feat: Hobbies components implemented

---
+++ my-app/data/Cards.ts
@@ -0,0 +1,52 @@
+import { CardProps } from "../components/Card";
+
+export const cards: CardProps[] = [
+    {
+        info: "Medaka Box",
+        image: require("../assets/images/medaka-box.jpg")
+    },
+    {
+        info: "One Piece",
+        image: require("../assets/images/one-piece.jpg")
+    },
+    {
+        info: "D Gray Man",
+        image: require("../assets/images/d-gray-man.jpg")
+    },
+    {
+        info: "Vagabond",
+        image: require("../assets/images/vagabond.jpg")
+    },
+    {
+        info: "Ajin",
+        image: require("../assets/images/ajin.jpg")
+    },
+    {
+        info: "All you need is kill",
+        image: require("../assets/images/all-you-need-is-kill.jpeg")
+    },
+    {
+        info: "20th Century Boys",
+        image: require("../assets/images/20th-century-boys.jpg")
+    },
+    {
+        info: "Dr Stone",
+        image: require("../assets/images/dr-stone.jpg")
+    },
+    {
+        info: "Dungeon Meshi",
+        image: require("../assets/images/dungeon-meshi.jpg")
+    },
+    {
+        info: "Berserk",
+        image: require("../assets/images/berserk.jpg")
+    },
+    {
+        info: "JoJo's part 7: Stell Ball Run",
+        image: require("../assets/images/jojo7.jpg")
+    },
+    {
+        info: "Shangri-La Frontier",
+        image: require("../assets/images/shangri-la.jpg")
+    }
+] 
