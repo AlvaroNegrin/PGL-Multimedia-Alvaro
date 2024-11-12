f4db8cd1c66189f5b4611ba508ba58a9e40a3eba
AlvaroNegrin <usr3856@salesianos-lacuesta.net>
2024-11-12 20:26:19 +0000

    feat: Add new icons to the tabBar

--- my-app/app/(tabs)/_layout.tsx
+++ my-app/app/(tabs)/_layout.tsx
@@ -2,15 +2,23 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
 import Description from '../../components/Description'
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { LIGHT_COLORS } from '../../styles/colors/color';
 
 const TabsLayout = () => {
   return (
     <View>
         <Description/>
         <View style={styles.container}>
-            <Tabs>
-                <Tabs.Screen name="hobbies" options={{headerShown: false}} />
-                <Tabs.Screen name="repository" options={{headerShown: false}} />
+            <Tabs screenOptions={{ tabBarActiveTintColor: LIGHT_COLORS.darkBlue}}>
+                <Tabs.Screen name="hobbies"  options={{headerShown: false, 
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="book" color={color} size={size} /> )
+                }} />
+                <Tabs.Screen name="repository" options={{headerShown: false,
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="qr-code-outline" color={color} size={size} /> )
+                }} />
             </Tabs>
         </View>
     </View>
