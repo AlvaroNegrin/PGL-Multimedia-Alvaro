9268504083f6a03eccb83ed6264e9dd01e94c9b1
AlvaroNegrin <usr3856@salesianos-lacuesta.net>
2024-11-23 18:01:23 +0000

    feat:
    Creating shopping page and a buttons in the welcome page

---
+++ my-app/app/shoppinglist/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ShoppingPage = () => {
+  return (
+    <View>
+      <Text>Mi Lista</Text>
+    </View>
+  )
+}
+
+export default ShoppingPage
+
+const styles = StyleSheet.create({})
