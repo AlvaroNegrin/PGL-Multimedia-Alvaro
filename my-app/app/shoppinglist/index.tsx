1e48f60a8d5246b5394d031b52857f0a6e492a77
AlvaroNegrin <usr3856@salesianos-lacuesta.net>
2024-11-24 15:17:24 +0000

    feat;
    Creating a flatlist in shoppinlist that acess to a component product with some manual data in it

--- my-app/app/shoppinglist/index.tsx
+++ my-app/app/shoppinglist/index.tsx
@@ -1,14 +1,105 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { LIGHT_COLORS } from '../../styles/colors/color';
+import { products } from '../../data/Products';
+import Product from '../../components/Product';
 
 const ShoppingPage = () => {
   return (
-    <View>
-      <Text>Mi Lista</Text>
+    <View style={styles.container}>
+      <View style={styles.titleWrapper}>
+          <Text style={styles.title}>Lista de la compra</Text>
+      </View>
+      <View style={styles.priceWrapper}>
+        <Text style={styles.price}>Precio total: 0€</Text>
+      </View>
+      <View style={styles.productsContainer}>
+        {products.length == 0 ?  (
+          <Text style={styles.emptyMessage}>La lista está vacía...</Text>
+        ) : (
+          <FlatList
+              data={products}
+              renderItem={({ item }) => (
+                <Product
+                  id = {item.id}
+                  name={item.name}
+                  category={item.category}
+                  udPrice={item.udPrice}
+                  quantity={item.quantity}
+                  isObtained={item.isObtained}
+                />
+              )}
+              keyExtractor={(item) => item.id}
+            />
+        )
+        }
+        <Pressable style={styles.addButton} >
+          <Text style={styles.addButtonText}>Añadir producto</Text>
+        </Pressable>
+      </View>
     </View>
-  )
+  );
 }
 
 export default ShoppingPage
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 15,
+        flexDirection: "column",
+        width: "100%" 
+        
+        
+    },
+    titleWrapper: {
+        alignContent: "center",
+        alignItems: "center",
+        
+        flex: 1
+    },
+    title: {
+      color: LIGHT_COLORS.lightPink,
+      fontSize: 34
+    },
+    priceWrapper: {
+      backgroundColor: LIGHT_COLORS.lightBlue,
+      justifyContent: "center",
+      flex: 1
+    },
+    price: {
+      color: LIGHT_COLORS.white,
+      fontSize: 24,
+      marginLeft: 10
+    },
+    productsContainer: {
+      borderColor: LIGHT_COLORS.darkBlue,
+      borderWidth: 3,
+      borderRadius: 20,
+      flex: 13,
+      width: "95%",
+      height : "95%",
+      margin : "auto",
+      justifyContent: "center"
+      
+    },
+    emptyMessage: {
+      fontSize: 24,
+      color: LIGHT_COLORS.darkBlue,
+      textAlign: "center"
+    },
+    addButton: {
+      position: 'absolute',
+      bottom: 20,
+      left: 20,
+      right: 20,
+      backgroundColor: LIGHT_COLORS.lightPink,
+      paddingVertical: 15,
+      borderRadius: 8,
+      alignItems: 'center',
+    },
+    addButtonText: {
+      color: LIGHT_COLORS.white,
+      fontWeight: 'bold',
+      fontSize: 16,
+    },
+})
---
+++ my-app/components/Product.tsx
@@ -0,0 +1,89 @@
+import { Image, ImageSourcePropType, Pressable, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { LIGHT_COLORS } from '../styles/colors/color';
+
+
+export type ProductProps = {
+    id: string,
+    name : string,
+    category: ImageSourcePropType,
+    udPrice: number,
+    quantity: number,
+    isObtained: boolean;
+  }
+const Product = ({
+    id,
+    name,
+    category,
+    udPrice,
+    quantity,
+    isObtained
+}: ProductProps) => {
+  return (
+    <View style={styles.productContainer}>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={category} />
+      </View>
+      <View style={styles.info}>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.details}>Cantidad: {quantity}</Text>
+        <Text style={styles.details}>{udPrice}€ ud</Text>
+        <Text style={styles.details}>En Carrito: {isObtained ? 'Sí' : 'No'}</Text>
+        <Pressable style={styles.deleteButton}>
+          <Text style={styles.deleteButtonText}>Eliminar</Text>
+        </Pressable>
+      </View>
+    </View>
+  )
+}
+
+export default Product
+
+const styles = StyleSheet.create({
+    productContainer: {
+        flexDirection: 'row',
+        borderWidth: 2,
+        borderColor: '#73A9E5',
+        borderRadius: 15,
+        paddingVertical: 2,
+        paddingHorizontal: 10,
+        marginVertical: 5,
+        marginHorizontal: 10,
+        backgroundColor: LIGHT_COLORS.lightBlue,
+      },
+      info: {
+        margin: 10,
+      },
+      name: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: LIGHT_COLORS.darkBlue,
+      },
+      details: {
+        fontSize: 14,
+        color: LIGHT_COLORS.darkBlue,
+      },
+      imageContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      },
+      image: {
+        width: 120,
+        height: 120,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: LIGHT_COLORS.darkBlue,
+      },
+      deleteButton: {
+        paddingVertical: 5,
+        paddingHorizontal: 15,
+        backgroundColor: LIGHT_COLORS.lightPink,
+        borderRadius: 8,
+        marginTop: 5
+      },
+      deleteButtonText: {
+        color: 'white',
+        fontWeight: 'bold',
+      },
+})
