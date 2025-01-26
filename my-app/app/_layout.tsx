387dff09ae7a32fe8dbd662c111947cc21538835
AlvaroNegrin <usr3856@salesianos-lacuesta.net>
2025-01-26 19:47:55 +0000

    refact: Change the error message location of the log function and Toast location

--- my-app/app/_layout.tsx
+++ my-app/app/_layout.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { Slot } from 'expo-router'
+import Toast from 'react-native-toast-message'
 
 const AppLayout = () => {
   return (
     <GestureHandlerRootView style={{flex:1}}>
         <Slot/>
+        <Toast/>
     </GestureHandlerRootView>
   )
 }
